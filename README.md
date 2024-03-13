d9d8d2528da36f5cf8f934a3b5a82af8a0a80a6f
Paulina <paulina.dabrovska@gmail.com>
2024-03-14 00:24:06 +0100

    ReadMe update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-Demo link:
+## About The SpotifyStats:
+It is a responsive web app, which allows the user to check their Spotify wrapped. Made with Spotify API.
+## Link to the website:
 Access my site at https://spotify-stats24.web.app/
+## Table of Content:
+<ul>
+  <li>
+    <a href="#about-the-SpotifyStats">About the App</a>
+  </li>
+  <li>
+    <a href="#screenshots">Screenshots</a>
+  </li>
+  <li>
+    <a href="#technologies">Technologies and tools</a>
+  </li>
+</ul>
+
+## Screenshots
+
+<img src="/images/screenshots/screen1.png">
+<img src="/images/Screenshots/screen2.png">
+
+## Technologies and tools
+I used:
+  <ul>
+    <li>React.js</li>
+    <li>axios</li>
+    <li>Tailwind CSS</li>
+    <li>Visual Studio Code</li>
+    <li>Figma</li>
+    <li>Firebase</li>
+    <li>GIT</li>
+  </ul>
+ html, css, Javascript, Visual Studio Code, Figma.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,37 +7,37 @@ export const Navbar = () => {
   const closeMenu = () => {setIsOpen(!isOpen)}
 
   return (
-    <div className={isOpen ? 'Navbar absolute top-0 left-0 w-full smX:w-[35vw] md:w-[22vw] min-h-screen text-lg bg-zinc-800/[.06] backdrop-blur-md z-[10]' : 'absolute' }>
+    <div className={isOpen ? 'Navbar z-10000 absolute top-0 left-0 w-full smX:w-[35vw] md:w-[22vw] min-h-screen text-lg bg-zinc-800/[.06] backdrop-blur-md' : 'absolute' }>
       <div onClick={closeMenu} className={isOpen? 'burger-menu close-menu mt-10 ml-9 mb-4 bg-zinc-800/[.0] ' : 'burger-menu ml-4 mb-4'}>
         <div id="line1"></div>
         <div id="line2"></div>
         <div id="line3"></div>
       </div>
-      <div className={isOpen ? 'flex flex-col items-center mt-5 bg-inherit' : 'hidden'}>
+      <div className={isOpen ? 'flex flex-col items-center mt-5 bg-zinc-800/[.0]' : 'hidden'}>
         <Link
           className='bg-zinc-800/[.06]'
           to={'/'}
           onClick={closeMenu}
         >
           <div
-            className='container p-6 bg-zinc-800/[.0] relative w-[180px] text-center'
+            className='container p-6 bg-inherit relative w-[180px] text-center'
           >
             <p
-              className='bg-zinc-800/[.0]'
+              className='bg-inherit'
             >Main</p>
             <div className='horizontal-line absolute bottom-[17%] right-[90%] left-[10%] h-[2px] bg-zinc-300 transition-right duration-500 linear'></div>
           </div>
         </Link>
         <Link
-          className='bg-zinc-800/[.06]'
+          className='bg-inherit'
           to={'/about'}
           onClick={closeMenu}
         >
           <div
-            className='container p-6 bg-zinc-800/[.0] relative w-[180px] text-center'
+            className='container p-6 bg-inherit relative w-[180px] text-center'
           >
             <p
-              className='bg-zinc-800/[.0]'
+              className='bg-inheerit'
             >About</p>
             <div className='horizontal-line absolute bottom-[17%] right-[90%] left-[10%] h-[2px] bg-zinc-300 transition-right duration-500 linear'></div>
           </div>
