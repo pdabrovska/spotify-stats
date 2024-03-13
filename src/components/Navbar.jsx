e5743c41799b0e86f6adee4d85cbe6dc53d435b5
Paulina <paulina.dabrovska@gmail.com>
2024-03-14 00:36:37 +0100

    layout update 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   const closeMenu = () => {setIsOpen(!isOpen)}
 
   return (
-    <div className={isOpen ? 'Navbar z-10000 absolute top-0 left-0 w-full smX:w-[35vw] md:w-[22vw] min-h-screen text-lg bg-zinc-800/[.06] backdrop-blur-md' : 'absolute' }>
+    <div className={isOpen ? 'Navbar z-[10000] absolute top-0 left-0 w-full smX:w-[35vw] md:w-[22vw] min-h-screen text-lg bg-zinc-800/[.06] backdrop-blur-md' : 'absolute' }>
       <div onClick={closeMenu} className={isOpen? 'burger-menu close-menu mt-10 ml-9 mb-4 bg-zinc-800/[.0] ' : 'burger-menu ml-4 mb-4'}>
         <div id="line1"></div>
         <div id="line2"></div>
@@ -37,7 +37,7 @@ export const Navbar = () => {
             className='container p-6 bg-transparent relative w-[180px] text-center'
           >
             <p
-              className='bg-inheerit'
+              className='bg-transparent'
             >About</p>
             <div className='horizontal-line absolute bottom-[17%] right-[90%] left-[10%] h-[2px] bg-zinc-300 transition-right duration-500 linear'></div>
           </div>
