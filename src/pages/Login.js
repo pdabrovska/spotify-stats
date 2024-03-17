9b1e7c0837fed8c198cb7daea2b4efb51eda498d
Paulina <paulina.dabrovska@gmail.com>
2024-03-17 18:13:04 +0100

    uri changed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,7 @@ import {motion} from 'framer-motion';
 
 export const Login = () => {
   const client_id = '132a9595d7f0453eab534d3118ac4c6d';
-  const redirect_uri = 'http://localhost:3000';
+  const redirect_uri = 'https://spotify-stats24.web.app/';
   const auth_endpoint = 'https://accounts.spotify.com/authorize';
   const response_type ='token';
   const scope = 'user-top-read';
